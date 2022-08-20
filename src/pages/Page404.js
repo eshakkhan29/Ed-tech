609c30d274836f28823548503c261dd72a69ef9d
Eshak Khan <92074702+eshakkhan29@users.noreply.github.com>
2022-08-21 01:36:37 +0600

    404 page add

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import page404 from '../images/404pages.jpg';
 
 const Page404 = () => {
     return (
-        <div>
-            <h1>Page 404</h1>
+        <div className='lg:w-8/12 mx-auto px-2 lg:px-0'>
+            <img src={page404} alt="" />
+            <div className='text-center'>
+                <Link className='text-xl text-white py-2 px-6 bg-secondary rounded-lg' to='/'>Back to Home</Link>
+            </div>
         </div>
     );
 };
